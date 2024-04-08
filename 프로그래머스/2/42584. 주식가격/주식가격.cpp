62d24219e19a82508dec1ce55b9e103d317d014b
yo0bin <55120730+KimYooBin1@users.noreply.github.com>
2024-04-08 11:09:20 +0900

    [level 2] Title: 주식가격, Time: 15.48 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+    vector<int> answer(prices.size(), 0);
+    stack<int> s;
+    for(int i=0;i<prices.size();i++){
+        while(!s.empty() && prices[s.top()] > prices[i]){
+            answer[s.top()] = i - s.top();
+            s.pop();
+        }
+        s.push(i);
+    }
+    while(!s.empty()){
+        answer[s.top()] = prices.size() - s.top() - 1;
+        s.pop();
+    }
+    return answer;
+}
