c5ec002d0c68b2197d5a184f473948ffbf67f4ba
yo0bin <55120730+KimYooBin1@users.noreply.github.com>
2024-04-24 14:36:46 +0900

    [Gold III] Title: 웜홀, Time: 20 ms, Memory: 2332 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.cc
+++ 백준/Gold/1865. 웜홀/웜홀.cc
@@ -6,19 +6,6 @@
 #include<tuple>
 #define FIO cin.tie(NULL); ios_base::sync_with_stdio(false);
 using namespace std;
-int parent[501];
-int find(int x){
-    if(x == parent[x]) return x;
-    return find(parent[x]);
-}
-
-bool U(int x, int y){
-    int p_x = find(x);
-    int p_y = find(y);
-    if(p_x == p_y) return true;
-    parent[p_y] = p_x;
-    return false;
-}
 
 
 int main(){
@@ -30,8 +17,7 @@ int main(){
         cin >> n >> m >> w;
         int dist[n+1];
         for (int i = 1; i <= n; i++) {
-            dist[i] = INT_MAX;
-            parent[i] = i;
+            dist[i] = 0;
         }
         vector<tuple<int, int, int>> v;
         for (int i = 0; i < m; i++) {
@@ -39,10 +25,6 @@ int main(){
             cin >> from >> to >> cost;
             v.push_back(make_tuple(from, to, cost));
             v.push_back(make_tuple(to, from, cost));
-            U(from, to);
-        }
-        for (int i = 1; i <= n; i++) {
-            dist[find(i)] = 0;
         }
         for (int i = 0; i < w; i++) {
             int from, to, cost;
@@ -56,7 +38,6 @@ int main(){
                 to = get<1>(v[j]);
                 cost = get<2>(v[j]);
 //                cout << from << " " << to << " " << cost << "\n";
-                if(dist[from] == INT_MAX) continue;
                 if(dist[from] + cost < dist[to]) dist[to] = dist[from] + cost;
 //                cout << dist[to] << "\n";
             }
@@ -65,8 +46,7 @@ int main(){
         for (int j = 0; j < v.size(); j++) {
             from = get<0>(v[j]);
             to = get<1>(v[j]);
-            cost = get<2>(v[j]);
-            if(dist[from] == INT_MAX) continue;
+            cost = get<2>(v[j]); 
             if(dist[from] + cost < dist[to]) {
                 check = false;
                 break;
