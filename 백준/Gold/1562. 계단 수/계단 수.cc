bdde09af37925394868fdfef8c1a08d141c2ce6a
KIM YU BIN <55120730+KimYooBin1@users.noreply.github.com>
2024-06-11 16:02:24 +0900

    [Gold I] Title: 계단 수, Time: 0 ms, Memory: 2052 KB -BaekjoonHub

--- 백준/Gold/1562. 계단 수/계단 수.cc
+++ 백준/Gold/1562. 계단 수/계단 수.cc
@@ -6,14 +6,13 @@
 typedef long long ll;
 using namespace std;
 int n;
-ll dist[10][101][1024];
+ll dist[10][101][4];
 int calc(int index, int bit, int num){
-//    cout<<index<<" "<<num<<" "<<bit<<"\n";
 
     if(dist[num][index][bit] != -1) return dist[num][index][bit];
     if(index == n){
         //0~9까지 모든 숫자를 사용했으면 1 return
-        if(bit == pow(2,10)-1) return 1;
+        if(bit == 3) return 1;
         //아니면 0 return
         return 0;
     }
@@ -22,12 +21,12 @@ int calc(int index, int bit, int num){
     //음수가 들어갈수는 없다
     if(pre_num != 0){
         int next_num2 = pre_num-1;
-        result += calc(index + 1, bit | (1 << next_num2), next_num2);
+        result += calc(index + 1, bit | (next_num2 == 0 ? 1 : 0), next_num2);
     }
     //한자리수씩 추가 가능하다
     if(pre_num != 9){
         int next_num1 = pre_num+1;
-        result += calc(index + 1, bit | (1 << next_num1), next_num1);
+        result += calc(index + 1, bit | (next_num1 == 9 ? 2 : 0), next_num1);
     }
     return dist[num][index][bit] = result % mod;
 }
@@ -37,7 +36,7 @@ int main(){
     cin >> n;
     ll result = 0;
     for(int i=1;i<10;i++){
-        result += calc(1, (1<<i), i)%mod;
+        result += calc(1, (i == 9) ? 2 : 0, i) % mod;
     }
     cout<<result%mod<<"\n";
 }
