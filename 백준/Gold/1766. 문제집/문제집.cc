22b6fad4fc3080645937cdf3457f7aa091faf168
KIM YU BIN <55120730+KimYooBin1@users.noreply.github.com>
2024-06-15 15:11:59 +0900

    [Gold II] Title: 문제집, Time: 356 ms, Memory: 4084 KB -BaekjoonHub

---
+++ 백준/Gold/1766. 문제집/문제집.cc
@@ -0,0 +1,53 @@
+#include <cstring>
+#include <iostream>
+#include <queue>
+#define FIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define MAX_SIZE 32001
+using namespace std;
+int cnt[MAX_SIZE];
+//0 아직 사용 못함, 1후보 등록 가능 2 사용됨
+int check[MAX_SIZE];
+int main(){
+    FIO;
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> v(MAX_SIZE, vector<int> ());
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        cin>>a>>b;
+        v[a].push_back(b);
+        cnt[b]++;
+    }
+    queue<int> q;
+    for (int i = 1; i <= n; i++) {
+        if(!cnt[i]){
+            check[i] = 1;
+        }
+    }
+    for(int i=1;i<=n;i++){
+        if(check[i] == 1){
+            q.push(i);
+            check[i] = 2;
+            break;
+        }
+    }
+    while(!q.empty()){
+        int x = q.front();
+        cout << x << " ";
+        q.pop();
+        for (int j = 0; j < v[x].size(); j++) {
+            int nx = v[x][j];
+            if(check[nx]) continue;
+            if(--cnt[nx] != 0) continue;
+            check[nx] = 1;
+        }
+        for (int i = 1; i <= n; i++) {
+            if(check[i] == 1){
+                q.push(i);
+                check[i] = 2;
+                break;
+            }
+        }
+    }
+    return 0;
+}
