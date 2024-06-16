71460172f2d2fa4e9b2ac8c675effb6c9a847f8f
KIM YU BIN <55120730+KimYooBin1@users.noreply.github.com>
2024-06-16 13:33:47 +0900

    [Gold V] Title: 수 나누기 게임, Time: 788 ms, Memory: 7268 KB -BaekjoonHub

---
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.cc
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<cstring>
+#include<vector>
+#include<algorithm>
+#include<map>
+#define FIO cin.tie(NULL); ios_base::sync_with_stdio(false);
+#define MAX_SIZE 100001
+using namespace std;
+bool check[MAX_SIZE];
+int main(){
+    FIO;
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    // 해당 num에 대한 점수
+    map<int, int> m;
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    for (int i = 0; i < n; i++) {
+        m[v[i]] = 0;
+    }
+    int max_element = *std::max_element(v.begin(), v.end());
+    for (int i = 1; i <= max_element; i++) {
+        //소수가 아님
+//        if(check[i]) continue;
+//        cout << i << "\n";
+        for (int j = i * 2; j <= max_element; j += i) {
+            // 양 숫자가 모두 v에 존재할때
+//            cout << i << " " << j << "\n";
+            if (m.find(i) != m.end() && m.find(j) != m.end()) {
+                m[i]++;
+                m[j]--;
+            }
+//            check[j] = true;
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        cout<<m[v[i]]<<" ";
+    }
+    cout << "\n";
+    return 0;
+}
