c0d4f7f66aedf30674ad85f63943270df86962f4
KIM YU BIN <55120730+KimYooBin1@users.noreply.github.com>
2024-06-17 14:54:50 +0900

    [Gold II] Title: 공항, Time: 12 ms, Memory: 3192 KB -BaekjoonHub

---
+++ 백준/Gold/10775. 공항/공항.cc
@@ -0,0 +1,39 @@
+#include <cstring>
+#include <iostream>
+#define FIO cin.tie(NULL); cout.tie(NULL); ios_base::sync_with_stdio(false);
+#define MAX_SIZE 100001
+using namespace std;
+int arr[MAX_SIZE];
+int next_gate[MAX_SIZE];
+int g_size[MAX_SIZE];
+int find(int x) {
+	if (next_gate[x] == x) return x;
+	return next_gate[x] = find(next_gate[next_gate[x]]);
+}
+void U(int x, int y) {
+	int nx = find(x);
+	int ny = find(y);
+	if (nx == ny) return;
+	if (ny > nx) swap(nx, ny);
+	next_gate[nx] = ny;
+}
+int main() {
+	FIO;
+	int g, p, to;
+	cin >> g >> p;
+	for (int i = 1; i <= p; i++) {
+		cin >> arr[i];
+	}
+	for (int i = 1; i <= g; i++) {
+		next_gate[i] = i;
+	}
+	int result = 0;
+	for (int i = 1; i <= p; i++) {
+		int gate = find(arr[i]);
+		if (gate == 0) break;
+		U(gate, gate - 1);
+		result++;
+	}
+	cout << result << "\n";
+	return 0;
+}
