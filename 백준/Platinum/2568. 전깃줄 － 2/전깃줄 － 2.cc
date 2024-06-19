c342a629380bc2eb8fb80e1ff3e7427e051ad5d4
KIM YU BIN <55120730+KimYooBin1@users.noreply.github.com>
2024-06-19 14:24:51 +0900

    [Platinum V] Title: 전깃줄 - 2, Time: 36 ms, Memory: 4592 KB -BaekjoonHub

--- 백준/Platinum/2568. 전깃줄 － 2/전깃줄 － 2.cc
+++ 백준/Platinum/2568. 전깃줄 － 2/전깃줄 － 2.cc
@@ -7,7 +7,6 @@
 using namespace std;
 vector<pair<int,int>> v;
 int dir[MAX_SIZE];
-bool check[MAX_SIZE];
 int low_bound(int s, int e, int target, vector<int> &vec){
     int result = e;
     while(s<e){
@@ -20,34 +19,40 @@ int low_bound(int s, int e, int target, vector<int> &vec){
             e = m;
         }
     }
-    return e;
+    return result;
 }
 int main(){
     FIO;
     int n;
     cin >> n;
     int a, b;
+    //전기줄 입력
     for (int i = 0; i < n; i++) {
         cin >> a >> b;
         v.push_back({a, b});
     }
+    //정렬을 통해 b만 확인해도 교차하는지 아닌지 판단하기 위해
     sort(v.begin(), v.end());
     vector<int> result;
+    //
     for (int i=0;i<v.size();i++){
         auto tmp = v[i];
-        //tmp 보다 큰값이 들어가 있음
+        //tmp 보다 크거나 같은 숫자의 index 출력
         int index = low_bound(0, result.size(), tmp.second, result);
+        //해당 위치가 가장 긴 순열에서 어느 index에 위치하는지
         dir[i] = index + 1;
+        // index가 가장 긴 부분순열 중간에 위치하면 해당 위치를 교체
         if(index != result.size()){
             result[index] = tmp.second;
         }
-        //tmp.second가 가장 큰수
+        // 현재 b가 가장 큰 숫자, 그냥 push
         else{
             result.push_back(tmp.second);
         }
     }
     int size = result.size();
     vector<int> ret;
+    // 가장 긴 순열에서 가장 마지막 index부터 ret에 추가해야 올바른 집합이 나온다
     for (int i = v.size() - 1; 0 <= i; i--) {
         if(dir[i] == size){
             size--;
