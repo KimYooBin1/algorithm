b15c3ebc95a44afcd09ff091d6e92597637b770b
KIM YU BIN <55120730+KimYooBin1@users.noreply.github.com>
2024-09-09 14:26:02 +0100

    [level 4] Title: 서울에 위치한 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+SELECT I.REST_ID, I.REST_NAME, I.FOOD_TYPE, I.FAVORITES, I.ADDRESS, ROUND(avg(R.REVIEW_SCORE), 2) as SCORE
+from REST_INFO as I
+join REST_REVIEW as R
+on I.REST_ID = R.REST_ID
+where SUBSTRING(I.ADDRESS,1,2) = "서울"
+group by I.REST_ID
+order by avg(R.REVIEW_SCORE) desc, sum(I.FAVORITES) desc
