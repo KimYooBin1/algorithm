ceddf31bcd58acca7e60da4d6f477d1113d82472
KIM YU BIN <55120730+KimYooBin1@users.noreply.github.com>
2024-11-09 11:46:50 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
+++ 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -1,20 +1,15 @@
 with temp as (
-    select id, RANK() over (order by size_of_colony desc) r, (select count(*) from ecoli_data) c
+    select id, PERCENT_RANK() over (order by size_of_colony desc) rate
     from ecoli_data
-),
-rating as (
-    select id, r / c rate
-    from temp
 )
-
 select id, (
-    case when 0 < rate and rate <= 0.25 then "CRITICAL" else 
-        case when rate <= 0.5 then "HIGH" else
-            case when rate <= 0.75 then "MEDIUM" else "LOW"
+    case when 0 <= rate and rate < 0.25 then "CRITICAL" else 
+        case when rate < 0.5 then "HIGH" else
+            case when rate < 0.75 then "MEDIUM" else "LOW"
             end
         end
     end
 ) colony_name
 from ecoli_data
-join rating using (id)
+join temp using (id)
 order by id;
