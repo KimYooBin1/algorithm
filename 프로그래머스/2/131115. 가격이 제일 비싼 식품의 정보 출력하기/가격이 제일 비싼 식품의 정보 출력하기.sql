824ef7f891bb287f2317cb60950879d3a10e154d
KIM YU BIN <55120730+KimYooBin1@users.noreply.github.com>
2024-11-10 11:52:29 +0900

    [level 2] Title: 가격이 제일 비싼 식품의 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131115. 가격이 제일 비싼 식품의 정보 출력하기/가격이 제일 비싼 식품의 정보 출력하기.sql
+++ 프로그래머스/2/131115. 가격이 제일 비싼 식품의 정보 출력하기/가격이 제일 비싼 식품의 정보 출력하기.sql
@@ -1,5 +1,8 @@
+with temp as (
+    select max(price) p
+    from food_product
+)
+
 select product_id, product_name, product_cd, category, price
 from food_product
-group by product_id
-order by price desc
-limit 1
+join temp on p = price
