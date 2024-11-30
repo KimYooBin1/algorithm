e7fbd896625bb63af7a95385d575f706a7c2c20c
KIM YU BIN <55120730+KimYooBin1@users.noreply.github.com>
2024-11-30 11:53:00 +0900

    [level 4] Title: 징검다리, Time: 4.59 ms, Memory: 5.29 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.cpp
+++ 프로그래머스/4/43236. 징검다리/징검다리.cpp
@@ -1,42 +1,33 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
-#include <cstring>
 
 using namespace std;
 
-int solution(int distance, vector<int> rocks, int n) {
-    rocks.push_back(0);
-    rocks.push_back(distance);
-    sort(rocks.begin(), rocks.end());
-    int answer;
+int solution(int e, vector<int> rocks, int n) {
+    int answer = 0;
     int s = 0;
-    int e = distance;
-    int m;
-    while(s<=e){
-        m = (s+e)/2;
+    rocks.push_back(e);
+    sort(rocks.begin(), rocks.end());
+    while(s <= e){
+        int m = (s + e) / 2;
         int cnt = 0;
-        int before = rocks[0];
-        //시작점과 도착점은 지울수 없기때문에 뺀다
-        for(int i=1;i<rocks.size();i++){
-            //간격이 더 크커나 같으면 부술 필요가 없다
-            if(m <= rocks[i] - before){
-                before = rocks[i];
-            }
-            //간격이 더 작으면 부순다
-            else{
+        int end = 0;
+        for(int i=0;i<rocks.size();i++){
+            if(rocks[i] - end < m){
                 cnt++;
+                continue;
             }
+            end = rocks[i];
         }
-        //목표한 돌보다 적게 부숨
-        if(cnt <= n){
+        // 제거한 돌이 더 많다(거리가 너무 넓다)
+        if(cnt > n){
+            e = m - 1;
+        }
+        // 제거한 돌이 더 적거나 같다(거리가 너무 좁다)
+        else{
             s = m + 1;
             answer = m;
-        }
-        //목표한 돌보다 많음
-        else{
-            e = m-1;
         }
     }
     return answer;
