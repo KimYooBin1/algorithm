b09ae8f5f8aa9b7d8bd56843c8f5e8e44cd9bc30
KIM YU BIN <55120730+KimYooBin1@users.noreply.github.com>
2025-11-13 11:20:37 +0900

    [Silver III] Title: Young한 에너지는 부족하다, Time: 160 ms, Memory: 3192 KB -BaekjoonHub

---
+++ 백준/Silver/23561. Young한 에너지는 부족하다/Young한 에너지는 부족하다.cc
@@ -0,0 +1,15 @@
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    vector<int> v(n * 3);
+    for (int i = 0; i < n * 3; i++) {
+        cin >> v[i];
+    }
+    sort(v.begin(), v.end());
+    cout << v[2 * n - 1] - v[n] << "\n";
+}
