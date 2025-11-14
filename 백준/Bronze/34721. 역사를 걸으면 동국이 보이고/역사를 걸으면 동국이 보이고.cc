ad245549b4c0eb107d6a563ab26cdf68d2d21864
KIM YU BIN <55120730+KimYooBin1@users.noreply.github.com>
2025-11-14 23:45:21 +0900

    [Bronze V] Title: 역사를 걸으면 동국이 보이고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34721. 역사를 걸으면 동국이 보이고/역사를 걸으면 동국이 보이고.cc
@@ -0,0 +1,14 @@
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+#define FIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+typedef long long ll;
+int main(){
+    FIO;
+    int n;
+    cin >> n;
+    while(n--){
+        cout << "I love DGU\n";
+    }
+}
