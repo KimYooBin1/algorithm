8a6c8823df93241ed877dff0a4696af475a4657d
KIM YU BIN <55120730+KimYooBin1@users.noreply.github.com>
2025-11-21 11:18:06 +0900

    [Silver I] Title: 꿈틀꿈틀 호석 애벌레 - 기능성, Time: 0 ms, Memory: 2020 KB, Score: 25 point -BaekjoonHub

---
+++ 백준/Silver/20167. 꿈틀꿈틀 호석 애벌레 － 기능성/꿈틀꿈틀 호석 애벌레 － 기능성.cc
@@ -0,0 +1,60 @@
+#include <cstring>
+#include <iostream>
+#include <vector>
+using namespace std;
+#define FIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+int main() {
+    FIO;
+    int n, k;
+    cin >> n >> k;
+    vector<int> feed(n);
+    for (int i = 0; i < n; i++) {
+        cin >> feed[i];
+    }
+    vector<int> next_i(n, -1);
+    for (int i = 0; i < n; i++) {
+        int sum = 0;
+        for (int j = i; j < n; j++) {
+            sum += feed[j];
+            if(k <= sum){
+                next_i[i] = j;
+                break;
+            }
+        }
+    }
+
+    vector<int> energy(n, 0);
+    for (int i = 0; i < n - 1; i++) {
+        int next_index = next_i[i];
+        int sum = 0;
+        for (int j = i; j <= next_index; j++) {
+            sum += feed[j];
+        }
+        energy[i] = sum - k;
+    }
+
+
+    vector<int> dp(n + 1, 0);
+    for (int i = 0; i < n; i++) {
+        if(n >= i + 1){
+            dp[i + 1] = max(dp[i + 1], dp[i]);
+        }
+        if(n >= next_i[i] + 1){
+            dp[next_i[i] + 1] = max(dp[next_i[i] + 1], dp[i] + energy[i]);
+        }
+    }
+//    for(auto a : dp){
+//        cout << a << " ";
+//    }
+//    cout<<"\n";
+//    for(auto a : next_i){
+//        cout << a << " ";
+//    }
+//    cout<<"\n";
+//    for(auto a : energy){
+//        cout << a << " ";
+//    }
+//    cout<<"\n";
+    cout << dp[n] << "\n";
+};
